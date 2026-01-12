acea913d259629f9001ed3ca6cf0728459c35df0
Alfonso Patrón <c9alfonsopatron@hotmail.com>
2026-01-12 12:50:03 -0600

    initial commit - project base

---
+++ One Lap/One Lap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  One Lap
+//
+//  Created by Alfonso Patron on 12/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ One Lap/One Lap/One_LapApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_LapApp.swift
+//  One Lap
+//
+//  Created by Alfonso Patron on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct One_LapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
